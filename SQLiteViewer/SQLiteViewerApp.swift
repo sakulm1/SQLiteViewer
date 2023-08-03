d6c381d9ff13a0133c040180f0885e58d0318d41
Lukas Maile <lukasmaile@icloud.com>
2023-08-03 18:56:25 +0200

    Initial Commit

---
+++ SQLiteViewer/SQLiteViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SQLiteViewerApp.swift
+//  SQLiteViewer
+//
+//  Created by Lukas Maile on 03.08.23.
+//
+
+import SwiftUI
+
+@main
+struct SQLiteViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
